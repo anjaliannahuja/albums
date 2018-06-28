37b55c197335d8f5f679dc8986e1625223096cf4
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-06-28 09:41:27 -0500

    adding other files

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+  <View>
+    <Header headerText="Albums" />
+    <AlbumList />
+  </View>
+);
+
+// Render it
+AppRegistry.registerComponent('albums', () => App);
