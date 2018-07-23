967515351a8920775a1361ce96516f2c75218594
Anjali Ahuja <anjaliannahuja@gmail.com>
2018-07-22 23:46:30 -0500

    added scroll functionality

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -20,9 +20,9 @@ class AlbumList extends Component {
 
   render() {
     return (
-      <View>
+      <ScrollView>
         {this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />)}
-      </View>
+      </ScrollView>
     );
   }
 }
